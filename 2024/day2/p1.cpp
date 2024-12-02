44d1639f73fee22ce00c9efd9af28984df947efd
Neptunium93 <62119053+Neptunium931@users.noreply.github.com>
2024-12-02 09:13:41 +0000

    2024 day2 p1

---
+++ 2024/day2/p1.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string>
+split(const std::string &s, const std::string &delimiter)
+{
+  std::vector<std::string> tokens;
+  std::string s_copy = s; // Create a non-const copy of the input string
+  size_t pos = 0;
+  std::string token;
+  while ((pos = s_copy.find(delimiter)) != std::string::npos)
+  {
+    token = s_copy.substr(0, pos);
+    tokens.push_back(token);
+    s_copy.erase(0, pos + delimiter.length());
+  }
+  tokens.push_back(s_copy); // Add the remaining part of the string
+  return tokens;
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+    return 1;
+  }
+  std::ifstream file;
+  file.open(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "Error opening file: " << argv[1] << std::endl;
+    return 1;
+  }
+  std::string delimiter = " ";
+  std::string line;
+  int safe = 0;
+  while (std::getline(file, line))
+  {
+    bool isSafe = true;
+    std::vector<std::string> tokens = split(line, delimiter);
+    for (auto i = 2; i < tokens.size(); i++)
+    {
+      if (!isSafe)
+      {
+        continue;
+      }
+      
+      auto x = std::stoi(tokens[i-2]);
+      auto y = std::stoi(tokens[i-1]);
+      auto z = std::stoi(tokens[i]);
+      if ((x > y && y > z) || (x < y && y < z))
+      {
+        if (std::abs(x - y) < 4 && std::abs(y - z) < 4)
+        {
+            isSafe = true;
+            continue;
+        }
+      }
+      isSafe = false;
+    }
+    if (isSafe)
+    {
+        safe++;
+    }
+  }
+  std::cout << safe << std::endl;
+  return 0;
+}
